e97ef020424ba0cc1513b83e7a21e69d0166f8a3
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-05-31 09:56:46 +0100

    updated

--- starter/script.js
+++ starter/script.js
@@ -3,7 +3,7 @@
 //---------------Selecting elements----------------------------------
 // We can either use the querySelector or getElementById method
 
-const score0 = document.querySelector('#score-0'); // Since we are not working with classes, we put the # symbol in fron fo the id name or we use the get element by id function
+const score0 = document.querySelector('#score-0'); // Since we are not working with classes, we put the # symbol in front of the id name or we use the get element by id function
 const score1 = document.getElementById('score-1');
 const diceModal = document.querySelector('.dice');
 
@@ -16,14 +16,22 @@ const player1 = document.querySelector('.player--1');
 
 let currentScore = 0;
 let activePlayer = 0;
+const scores = [0, 0];
 
 // Starting conditions
-score0.textContent=0; score1.textContent = 0;                    // TThe two players total score is 0at the begining.
+score0.textContent=0; score1.textContent = 0;                    // TThe two players total score is 0 t the begining.
 diceModal.classList.add('hidden');                            // The dice should not be displayed until we roll it. Creating a hidden class in your css and setting the display property to none while adding it to the classList will hide that modal
+
+const switchPlayer = () => {
+activePlayer = activePlayer === 0 ? 1 : 0;   // else if the dice is '1', switch to the next player 
+currentScore = 0;
+player0.classList.toggle('player--active');   // The toggle method checks if the player class has the player active, if yes, it removes it but if no, it adds the player--active class to it
+player1.classList.toggle('player--active');
+}
+
 
 btnRoll.addEventListener('click', function(){
     const diceResult = Math.trunc(Math.random() * 6) + 1;  // Generate a random numner
-
     diceModal.classList.remove('hidden');                // Remove the hidden modal class
     diceModal.src=`dice-${diceResult}.png`;             // Display Dice Result
 
@@ -31,17 +39,21 @@ btnRoll.addEventListener('click', function(){
     currentScore += diceResult;                      // Then the currentScore keeps accumulating.                
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
      }else {
-        activePlayer = activePlayer ===0 ? 1 : 0;   // else if the dice is '1', switch to the next player 
-        currentScore = 0;
-        player0.classList.toggle('player--active');   // The toggle method checks if the player class has the player active, if yes, it removes it but if no, it adds the player--active class to it
-        player1.classList.toggle('player--active');
+        switchPlayer();
      };
 });
 
 btnHold.addEventListener('click', function(){
-    // Add current score to current player
-    activePlayer = activePlayer ===0 ? 1 : 0; // Switch to the next player
-    player0.classList.toggle('player--active');  
-})
+    scores[activePlayer] += currentScore;               // Add current score to current player
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer]; // Display the updated score
+
+    if (scores[activePlayer] >=20){// Finish the game
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');   // Add the winner class
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');  // Remove the 
+    }
+    else{
+        switchPlayer();
+    }
+});
 
 
